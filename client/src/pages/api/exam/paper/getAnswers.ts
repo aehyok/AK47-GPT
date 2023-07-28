8808f99719e83293b9aaa8e7af2921ed0e0ac3bf
刘启明 <455043818@qq.com>
2023-07-28 17:56:02 +0800

    fix:" update get paper Info

--- client/src/pages/api/exam/paper/getAnswers.ts
+++ client/src/pages/api/exam/paper/getAnswers.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const data = await ExamPaper.find(
       {
-        paperId
+        "_id":paperId
       }, '_id name level categoryId themeChoices score remark')
 
     const answers = await ExamAnswer.find({
