3ea220cb5030619e885283abe4234e924c127bef
刘启明 <455043818@qq.com>
2023-07-28 15:41:24 +0800

    fix: update getQuestions api

--- client/src/pages/api/exam/answer/update.ts
+++ client/src/pages/api/exam/answer/update.ts
@@ -28,9 +28,8 @@ async function gptTranslate(
                   面试者的回答：${answerContent}`;
   const chatCompletion = await chatApi.createChatCompletion({
     model: 'gpt-3.5-turbo-16k',
-    temperature: 0,
+    temperature: 0.6,
     messages: [{ role: 'user', content: `${prompt}` }],
-    function_call: "auto",
     functions: [
       {
         name: "write_evaluation_Info_db",
@@ -52,6 +51,7 @@ async function gptTranslate(
       }
     ],
   });
+  console.log(chatCompletion.data, '-000000000000000000-------------chatCompletion')
   console.log(chatCompletion.data.choices[0].message, 'chatCompletion')
   return chatCompletion.data.choices[0].message?.function_call?.arguments;
 }
--- client/src/pages/api/exam/paper/getQuestions.ts
+++ client/src/pages/api/exam/paper/getQuestions.ts
@@ -25,16 +25,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     where = { paperId }
     const questionList = await ExamAnswer.find(
       where,
-      'questionId'
+      'questionId score answerContent'
     )
 
     let ids = questionList.map(item => item.questionId);
     console.log(ids, '-------codes-------------')
     console.log(questionList, '----questionList----')
-    const result = await ExamQuestion.find({ _id: {$in: ids }})
+    const result: any = await ExamQuestion.find({ _id: {$in: ids }})
+
+    const newResult: any = []
+    result.forEach((item: any) => {
+      console.log(item._id, item._id.toString(), "----------------", "1")
+      const current: any = questionList.find(question => question.questionId.toString() === item._id.toString())
+      console.log( current, 'item-current')
+      if(current && current.score >= 0) {
+        item.score = current.score
+        console.log(item, 'item-score')
+      }
+      newResult.push({
+        _id: item._id,
+        question: item.question,
+        isAnswered: item.score >= 0
+      })
+    })
+
+    console.log(newResult, 'item-result')
     jsonRes<any>(res, {
       data: {
-        data: result,
+        data: newResult,
       }
     });
 
